1729bdf107f59bdefa9e66cb84805fd8aab09ea7
Your datco <datco2310@gmail.com>
2024-02-06 13:50:22 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,26 @@
+// function Xuandat(){
+//     let number=0
+//     function Con(){
+//         return ++number
+//     }
+//     return Con
+// }
+
+// var Co=Xuandat()
+// console.log(Co())
+// console.log(Co())
+// console.log(Co())
+// console.log(Co())
+
+
+// function Xuandat(age){
+//     function COn(name){
+//         console.log(`${name}` +' '+`${age}`)
+//     }
+//     return COn
+// }
+
+// var M=Xuandat('INFO')
+
+// console.log(M('toi la hieu lanh 18 '))
+
